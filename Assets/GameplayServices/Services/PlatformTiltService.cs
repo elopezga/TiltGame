1bb6a84d671d11f4859c3f950cc91f6d8fbf5631
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-09-08 15:13:37 -0700

    Fix tilt control not working for x-axis movement

--- Assets/GameplayServices/Services/PlatformTiltService.cs
+++ Assets/GameplayServices/Services/PlatformTiltService.cs
@@ -18,8 +18,6 @@ namespace TiltGame.GameplayServices
 
         void Start()
         {
-            /* _storiesHelper[0].Setup(gameObject, OnStoryChange);
-            _storiesHelper[1].Setup(gameObject, story => {Debug.Log(story.Get<Xbox360ControllerStory>().JoystickLeft);}); */
             _controllerStoriesHelper.Setup(gameObject, OnControllerStateChange);
             _gameplayStoriesHelper.Setup(gameObject, OnGameplayStateChange);
         }
@@ -39,26 +37,11 @@ namespace TiltGame.GameplayServices
 
         private void UpdateTilt()
         {
-            Vector3 projectedForward = Vector3.ProjectOnPlane(_cameraForward, Vector3.up);
+            Vector3 projectedForward = Vector3.Cross(_cameraRight, Vector3.up); // Project on plane does not work for some reason
             Vector3 projectedRight = Vector3.ProjectOnPlane(_cameraRight, Vector3.up);
 
             transform.rotation = Quaternion.AngleAxis(Joystick2RotationAngle(_joystickLeft.x), -1f * projectedForward)
                 * Quaternion.AngleAxis(Joystick2RotationAngle(_joystickLeft.y), projectedRight);
-        }
-
-        private void OnStoryChange(Story story)
-        {
-            /*Vector2 _joystickLeft = story.Get<Xbox360ControllerStory>().JoystickLeft;
-            Debug.Log(_joystickLeft);
-            Vector3 _cameraForward = story.Get<GameplayCameraStory>().CameraForward;
-            Vector3 _cameraRight = story.Get<GameplayCameraStory>().CameraRight;
-            
-
-            Vector3 projectedForward = Vector3.ProjectOnPlane(_cameraForward, Vector3.up);
-            Vector3 projectedRight = Vector3.ProjectOnPlane(_cameraRight, Vector3.up);
-
-            transform.rotation = Quaternion.AngleAxis(Joystick2RotationAngle(_joystickLeft.x), -1f * projectedForward)
-                * Quaternion.AngleAxis(Joystick2RotationAngle(_joystickLeft.y), projectedRight);  */
         }
 
         private float Joystick2RotationAngle(float joystickAxisValue)
