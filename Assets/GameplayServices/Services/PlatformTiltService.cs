68fc7020095f411208b85a97f3210e29cf566952
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-09-23 23:02:02 -0700

    Rudimentary tilt smoothening

--- Assets/GameplayServices/Services/PlatformTiltService.cs
+++ Assets/GameplayServices/Services/PlatformTiltService.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityStories;
 using TiltGame.ControllerInputServices;
 
@@ -9,10 +10,15 @@ namespace TiltGame.GameplayServices
         public StoriesHelper _controllerStoriesHelper;
         public StoriesHelper _gameplayStoriesHelper;
         public float TiltAngleMax = 15f;
+        public float TiltSlerpDuration = 0.2f;
 
         private Vector2 _joystickLeft = Vector2.zero;
         private Vector3 _cameraForward = Vector3.zero;
         private Vector3 _cameraRight = Vector3.zero;
+
+        // State
+        private Quaternion _targetRotation = Quaternion.identity;
+        private IEnumerator _tiltSlerpCoroutine = null;
 
         void Start()
         {
@@ -38,8 +44,39 @@ namespace TiltGame.GameplayServices
             Vector3 projectedForward = Vector3.Cross(_cameraRight, Vector3.up); // Project on plane does not work for some reason
             Vector3 projectedRight = Vector3.ProjectOnPlane(_cameraRight, Vector3.up);
 
-            transform.rotation = Quaternion.AngleAxis(Joystick2RotationAngle(_joystickLeft.x), -1f * projectedForward)
+            // Do Slerp for smoother movement
+            /* transform.rotation = Quaternion.AngleAxis(Joystick2RotationAngle(_joystickLeft.x), -1f * projectedForward)
+                * Quaternion.AngleAxis(Joystick2RotationAngle(_joystickLeft.y), projectedRight); */
+
+            _targetRotation = Quaternion.AngleAxis(Joystick2RotationAngle(_joystickLeft.x), -1f * projectedForward)
                 * Quaternion.AngleAxis(Joystick2RotationAngle(_joystickLeft.y), projectedRight);
+
+            // Only have one tilt slerp coroutine. Any input updates within the running coroutine will be processed within the same tilt slerp.
+            // Treat this as batch slerping inputs.
+            // Not a good solution because last minute input change withing already running coroutine will cause it to snap in that direction.
+            if (_tiltSlerpCoroutine == null)
+            {
+                _tiltSlerpCoroutine = TiltSlerp();
+                StartCoroutine(_tiltSlerpCoroutine);
+            }            
+
+            //transform.rotation = Quaternion.Slerp(sourceRotation, targetRotation, )
+        }
+
+        private IEnumerator TiltSlerp()
+        {
+            float elapsedTime = 0f;
+            Quaternion sourceRotation = transform.rotation;
+
+            while (elapsedTime < TiltSlerpDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                transform.rotation = Quaternion.Slerp(sourceRotation, _targetRotation, Mathf.InverseLerp(0f, TiltSlerpDuration, elapsedTime));
+
+                yield return 0f;
+            }
+
+            _tiltSlerpCoroutine = null;
         }
 
         private float Joystick2RotationAngle(float joystickAxisValue)
