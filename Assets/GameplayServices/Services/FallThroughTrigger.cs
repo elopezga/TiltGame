01149cbcaf857dcc85a2144e797a8105b6dfb619
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-09-28 17:21:01 -0700

    Add better hole implementation

---
+++ Assets/GameplayServices/Services/FallThroughTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class FallThroughTrigger : MonoBehaviour
+{
+    public event Action<Collider> TriggerEnter;
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if (TriggerEnter != null)
+        {
+            TriggerEnter.Invoke(otherCollider);
+        }
+    }
+}
--- Assets/GameplayServices/Services/GameplayHoleService.cs
+++ Assets/GameplayServices/Services/GameplayHoleService.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameplayHoleService : MonoBehaviour
+namespace TiltGame.GameplayServices
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameplayHoleService : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private string _platformFloorTag;
+        [SerializeField]
+        private FallThroughTrigger _fallThroughTrigger;
+        private MeshCollider _platformFloorCollider;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            GameObject platformFloor = GameObject.FindGameObjectWithTag(_platformFloorTag);
+            _platformFloorCollider = platformFloor.GetComponent<MeshCollider>();
+        }
+
+        private void OnEnable()
+        {
+            _fallThroughTrigger.TriggerEnter += CauseFallThroughPlatform;
+        }
+
+        private void OnDisable()
+        {
+            _fallThroughTrigger.TriggerEnter -= CauseFallThroughPlatform;
+        }
+
+        private void CauseFallThroughPlatform(Collider otherCollider)
+        {
+            Physics.IgnoreCollision(otherCollider, _platformFloorCollider, true);
+            Debug.LogError(string.Format("Ignore collisions between {0} and {1}", otherCollider.name, _platformFloorCollider.name));
+        }
     }
 }
